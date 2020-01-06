d3c23757ccc352e62cc7054cf575dd429a5dbb80
David Kislyak <dkislyak@mail.greenriver.edu>
2020-01-06 14:41:26 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    /* David Kislyak
+     * 1/6/20
+     * 328/pp1/index.php
+     * Pair Program 1
+     */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 1</title>
+</head>
+<body>
+    <h1>Pair Program 1</h1>
+</body>
+</html>
